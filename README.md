b5898e8ece016843210a419bbfa34a7a4d045b7d
Carsten Witt <carsten.witt@gmail.com>
2017-11-15 15:33:18 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Germania KG · Logger
+
+**Pimple Service Provider for Logging with Monolog.**
+
+
+## Installation
+
+```bash
+$ composer require germania-kg/logger
+```
+
+Alternatively, add this package directly to your *composer.json:*
+
+```json
+"require": {
+    "germania-kg/logger": "^1.0"
+}
+```
+
+
+## Usage
+
+
+```php
+<?php
+use Germania\Logger\LoggerServiceProvider;
+
+// ... to be done.
+```
+
+
+## Development
+
+```bash
+$ git clone https://github.com/GermaniaKG/Logger.git
+$ cd Logger
+$ composer install
+```
+
+
+## Unit tests
+
+Either copy `phpunit.xml.dist` to `phpunit.xml` and adapt to your needs, or leave as is. 
+Run [PhpUnit](https://phpunit.de/) like this:
+
+```bash
+$ vendor/bin/phpunit
+```
---
+++ src/LoggerServiceProvider.php
@@ -0,0 +1,196 @@
+<?php
+namespace Germania\ServiceProviders;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+use Monolog\Logger as Monolog;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\NullHandler;
+use Monolog\Processor\WebProcessor;
+use Bramus\Monolog\Formatter\ColoredLineFormatter;
+
+
+
+class LoggerServiceProvider implements ServiceProviderInterface
+{
+
+    public $logname;
+    public $loglevel;
+    public $logfile;
+    public $is_dev;
+    public $log_runtime;
+
+
+    /**
+     * @param string  $logname      The default Logger channel name
+     * @param string  $logfile      The logfile to use
+     * @param int     $loglevel     The log level
+     * @param bool    $is_dev       Wether this environment is "Development" or not
+     * @param bool    $log_runtime  Wether to log the script runtime
+     */
+    public function __construct( $logname, $logfile, $loglevel, $is_dev, $log_runtime )
+    {
+        $this->logname     = $logname;
+        $this->logfile     = $logfile;
+        $this->loglevel    = $loglevel;
+        $this->is_dev      = (bool) $is_dev;
+        $this->log_runtime = (bool) $log_runtime;
+    }
+
+
+
+    /**
+     * @implements ServiceProviderInterface
+     */
+    public function register(Container $dic)
+    {
+
+        $dic['Logger.loglevel'] = function($dic) {
+            return $this->loglevel;
+        };
+
+        $dic['Logger.logfile'] = function($dic) {
+            return $this->logfile;
+        };
+
+        $dic['Logger.name'] = function($dic) {
+            return $this->logname;
+        };
+
+
+
+
+
+
+
+        /**
+         * @return  RotatingFileHandler
+         */
+        $dic['Logger.Handler.RotatingFiles'] = function($dic) {
+            $loglevel = $dic['Logger.loglevel'];
+            $logfile  = $dic['Logger.logfile'];
+
+            // Writeable check
+            if ($logdir = dirname($logfile)
+            and !is_dir( $logdir)
+            and !mkdir($logdir, 0775, "recursive")):
+                http_response_code( 500 );
+                die("Could not create logfile directory.");
+            endif;
+
+            if (!is_writable( $logdir)):
+                http_response_code( 500 );
+                die("Logfile directory not writeable.");
+            endif;
+
+            return new RotatingFileHandler($logfile, 0, $loglevel);
+        };
+
+
+
+        /**
+         * @return StreamHandler|NullHandler
+         */
+        $dic['Logger.Handler.StdErr'] = function($dic) {
+            $loglevel = $dic['Logger.loglevel'];
+
+            if ($this->is_dev):
+                $stderr_handler = new StreamHandler('php://stderr', $loglevel);
+                $stderr_handler->setFormatter(new ColoredLineFormatter());
+                return $stderr_handler;
+            endif;
+
+            return new NullHandler( $loglevel );
+
+        };
+
+
+        /**
+         * @return array
+         */
+        $dic['Logger.Handlers'] = function($dic) {
+            return [
+                $dic['Logger.Handler.RotatingFiles'],
+                $dic['Logger.Handler.StdErr']
+            ];
+        };
+
+
+        /**
+         * @return array
+         */
+        $dic['Logger.Environment'] = function($dic) {
+            return $_SERVER;
+        };
+
+
+        /**
+         * @return array
+         */
+        $dic['Logger.Processor.Web.fields'] = function($dic) {
+            return [
+                'http_method',
+                'url',
+                'ip'
+            ];
+        };
+
+
+        /**
+         * @return WebProcessor
+         */
+        $dic['Logger.Processor.Web'] = function($dic) {
+            $server = $dic['Logger.Environment'];
+            $fields = $dic['Logger.Processor.Web.fields'];
+            return new WebProcessor( $server, $fields );
+        };
+
+
+        /**
+         * @return array
+         */
+        $dic['Logger.Processors'] = function($dic) {
+            return [
+                $dic['Logger.Processor.Web']
+            ];
+        };
+
+
+
+        /**
+         * @return Monolog
+         */
+        $dic['Logger'] = function($dic) {
+            $logname    = $dic['Logger.name'];
+            $handlers   = $dic['Logger.Handlers'];
+            $processors = $dic['Logger.Processors'];
+
+            return new Monolog( $logname, $handlers, $processors );
+        };
+
+
+
+        /**
+         * @param  float $start_time
+         * @return callable
+         */
+        $dic['Logger.LogRuntime'] = $dic->protect(function($start_time) use ($dic) {
+            $logger = $dic['Logger'];
+            $runtime_ms = ((microtime("float") - $start_time) * 1000);
+
+            $logger->info( "Finished", [ 'runtime' => $runtime_ms . "ms" ]);
+        });
+
+
+        // For statistic purposes
+        if ($this->log_runtime) :
+            $server  = $dic['Logger.Environment'];
+            $handler = $dic->raw('Logger.LogRuntime');
+
+            register_shutdown_function($handler, $server['REQUEST_TIME_FLOAT']);
+        endif;
+    }
+}
+
