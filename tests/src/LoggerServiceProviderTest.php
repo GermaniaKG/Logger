1c1d403809a0f989b4648265ee84ca69e571f95a
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2019-02-14 15:55:49 +0100

    Complete test

--- tests/src/LoggerServiceProviderTest.php
+++ tests/src/LoggerServiceProviderTest.php
@@ -5,15 +5,30 @@ use Germania\Logger\LoggerServiceProvider;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 use Monolog\Handler\AbstractHandler;
+use Psr\Log\LoggerInterface;
 
 class LoggerServiceProviderTest extends \PHPUnit\Framework\TestCase
 {	
 
-	public function testInstantiation()
+	/**
+	 * @dataProvider provideCtorArgs
+	 */
+	public function testInstantiation( $logname, $server, $anonymize )
 	{
 		
-		$sut = new LoggerServiceProvider("logname", array(), true);
+		$sut = new LoggerServiceProvider($logname, $server, $anonymize);
 		$this->assertInstanceOf( ServiceProviderInterface::class, $sut);
+	}
+
+	public function provideCtorArgs()
+	{
+		return array(
+			[ "logname", array(), true ],
+			[ "logname", array(), false ],
+			[ "logname", array( 'REMOTE_ADDR' => '127.0.0.1'), false ],
+			[ "logname", array( 'REMOTE_ADDR' => '127.0.0.1'), true ]
+			
+		);
 	}
 
 
@@ -27,6 +42,35 @@ class LoggerServiceProviderTest extends \PHPUnit\Framework\TestCase
 
 		$result = $container['Monolog.Handlers'];
 		$this->assertInternalType("array", $result);
+	}
+
+
+	public function testMonologProcessors( )
+	{
+
+		$sut = new LoggerServiceProvider("logname", array(), true);
+
+		$container = new Container;
+		$container->register( $sut );
+
+		$result = $container['Monolog.Processors'];
+		$this->assertInternalType("array", $result);
+	}
+
+
+	public function testLoggerInterface( )
+	{
+
+		$sut = new LoggerServiceProvider("logname", array(), true);
+
+		$container = new Container;
+		$container->register( $sut );
+
+		$result = $container['Logger'];
+		$this->assertInstanceOf(LoggerInterface::class, $result);
+
+		$result = $container['Monolog.Psr3Logger'];
+		$this->assertInstanceOf(LoggerInterface::class, $result);
 	}
 
 }
